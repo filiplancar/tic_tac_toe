0c35a4b897c21caa9692d84b936c873286f139d3
Filip Lancaric <filiplancaric222@gmail.com>
2022-07-14 12:11:12 +0200

    Add class Player

--- main.py
+++ main.py
@@ -17,44 +17,44 @@ class Field:
 field = Field(3,3)
 field.create_field()
 
-# class Player:
-#     def __init__(self, sign):
-#         self.sign = sign
-#         self._x = None
-#         self._y = None
+class Player:
+    def __init__(self, sign):
+        self.sign = sign
+        self._x = None
+        self._y = None
     
-#     #Getters
-#     @property
-#     def x(self):
-#         return self._x
+    #Getters
+    @property
+    def x(self):
+        return self._x
     
-#     @property
-#     def y(self):
-#         return self._y
+    @property
+    def y(self):
+        return self._y
 
-#     #Setters
-#     @x.setter
-#     def x(self, x):
-#         self._x = x
+    #Setters
+    @x.setter
+    def x(self, x):
+        self._x = x
     
-#     @y.setter
-#     def y(self, y):
-#         self._y = y
+    @y.setter
+    def y(self, y):
+        self._y = y
         
 
-# player = Player("O")
+player = Player("O")
 
-# while True:
-#     try:
-#         player.x = int(input("Zadaj x-súradnicu: "))
-#         player.y = int(input("Zadaj y-súradnicu: "))
-#         if player.x in range(field.width) and player.y in range(field.height):
-#             print("Správne súradnice")
-#             break
-#     except ValueError:
-#         print("Nerozumiem")
+while True:
+    try:
+        player.x = int(input("Zadaj x-súradnicu: "))
+        player.y = int(input("Zadaj y-súradnicu: "))
+        if player.x in range(field.width) and player.y in range(field.height):
+            print("Správne súradnice")
+            break
+    except ValueError:
+        print("Nerozumiem")
     
-#     print("Súradnice mimo rozsahu")    
+    print("Súradnice mimo rozsahu")    
 
 
 # ROBIM COUNTER PRE KOLÁ - JEDNO KOLO OBIDVAJA HRÁČI ŤAHAJÚ, POSLEDNÉ KOLO BUDE ŤAHAŤ LEN JEDEN HRÁČ
