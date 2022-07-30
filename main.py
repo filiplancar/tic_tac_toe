3c33aa3d5c7661d663724947b1b1158158576961
Filip Lancaric <filiplancaric222@gmail.com>
2022-07-30 13:17:46 +0200

    Handle creating of field

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 # print('Piškvorky')
 # print('Hráč 1')
 # print('Hráč 2')
-
 field = []
+empty_space = "_"
 class Field:
     def __init__(self, height, width):
         self.height = height
         self.width = width
     
     def create_field(self):
-        print(' ', end='')
-        for width in range(self.width*self.height):
-            print('',width, end='')
-        print()
+        print(" ", end = "")
+        for w in range(self.width):
+            print("", w, end="")
+        
+        print(" ")
+        for h in range(self.height):
+            field.append(f'{empty_space} '*self.width)
+            print(h, field[h])
             
-field = Field(3,3)
-field.create_field()
+f = Field(3,3)
+f.create_field()
 
 class Player:
     def __init__(self, sign):
@@ -42,20 +46,20 @@ class Player:
         self._y = y
         
 
-player = Player("O")
+# player = Player("O")
 
-while True:
-    try:
-        player.x = int(input("Zadaj x-súradnicu: "))
-        player.y = int(input("Zadaj y-súradnicu: "))
-        if player.x in range(field.width) and player.y in range(field.height):
-            print("Správne súradnice")
-            break
-    except ValueError:
-        print("Nerozumiem")
+# while True:
+#     try:
+#         player.x = int(input("Zadaj x-súradnicu: "))
+#         player.y = int(input("Zadaj y-súradnicu: "))
+#         if player.x in range(field.width) and player.y in range(field.height):
+#             print("Správne súradnice")
+#             break
+#     except ValueError:
+#         print("Nerozumiem")
     
-    print("Súradnice mimo rozsahu")    
+#     print("Súradnice mimo rozsahu")    
 
 
 # ROBIM COUNTER PRE KOLÁ - JEDNO KOLO OBIDVAJA HRÁČI ŤAHAJÚ, POSLEDNÉ KOLO BUDE ŤAHAŤ LEN JEDEN HRÁČ
-# PRÁVE SOM DO3IEL NA TO ŽE HRACIU PLOCHU MUSÍM SPRAVIŤ CEZ LIST
+# DOŠIEL SOM NA TO ŽE POLE MUSÍM SPRAVIŤ NEJAKO CEZ FOR CYKLUS, CEZ DVA FOR CYKLY - NAJSKOR VYRIESIM ABY SA MI VYPISOVALI INDEXY
